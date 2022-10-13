086fa9084773881996c648b434e1281176002087
BanjiBear <tommychen900217@gmail.com>
2022-10-13 21:19:03 +0800

    Ethics v 1.2 DONE

--- ethics.py
+++ ethics.py
@@ -8,6 +8,7 @@ from random import random
 """
 from random import randrange
 
+"""
 LINES_OF_POS_LOW_SCORE_OBSERVATION = 16
 LINES_OF_POS_MID_SCORE_OBSERVSTION = 23
 LINES_OF_POS_HIGH_SCORE_OBSERVATION = 22
@@ -17,52 +18,57 @@ LINES_OF_POS_LOW_SCORE_ASSESSMENT = 17
 LINES_OF_POS_MID_SCORE_ASSESSMENT = 15
 LINES_OF_POS_HIGH_SCORE_ASSESSMENT = 25
 TOTAL_LINES_OF_POS_ASSESSMENT = LINES_OF_POS_LOW_SCORE_ASSESSMENT + LINES_OF_POS_MID_SCORE_ASSESSMENT + LINES_OF_POS_HIGH_SCORE_ASSESSMENT
+"""
 
 class Ethics:
 
-	corpus = open("./ethicsPOS.txt", "r").readlines()
 	"""
 		Reference of initializing multiple list: 
 		https://stackoverflow.com/questions/2402646/python-initializing-multiple-lists-line
 	"""
-	observationLow, observationMid, observationHigh, assessmentLow, assessmentMid, assessmentHigh = ([] for i in range(6))
+	pos_obs_low, pos_obs_mid, pos_obs_high, pos_asses_low, pos_asses_mid, pos_asses_high = ([] for i in range(6))
 
 	def loadCorpus(self):
-		global LINES_OF_POS_LOW_SCORE_OBSERVATION, LINES_OF_POS_MID_SCORE_OBSERVSTION, LINES_OF_POS_HIGH_SCORE_OBSERVATION, TOTAL_LINES_OF_POS_OBSERVATION
-		self.observationLow = self.corpus[0 : LINES_OF_POS_LOW_SCORE_OBSERVATION]
-		self.observationMid = self.corpus[LINES_OF_POS_LOW_SCORE_OBSERVATION : LINES_OF_POS_LOW_SCORE_OBSERVATION + LINES_OF_POS_MID_SCORE_OBSERVSTION]
-		self.observationHigh = self.corpus[LINES_OF_POS_MID_SCORE_OBSERVSTION : LINES_OF_POS_MID_SCORE_OBSERVSTION + LINES_OF_POS_HIGH_SCORE_OBSERVATION]
-
-		global LINES_OF_POS_LOW_SCORE_ASSESSMENT, LINES_OF_POS_MID_SCORE_ASSESSMENT, LINES_OF_POS_HIGH_SCORE_ASSESSMENT, TOTAL_LINES_OF_POS_ASSESSMENT
-		self.assessmentLow = self.corpus[TOTAL_LINES_OF_POS_OBSERVATION : TOTAL_LINES_OF_POS_OBSERVATION + LINES_OF_POS_LOW_SCORE_ASSESSMENT]
-		self.assessmentMid = self.corpus[LINES_OF_POS_LOW_SCORE_ASSESSMENT : LINES_OF_POS_LOW_SCORE_ASSESSMENT + LINES_OF_POS_MID_SCORE_ASSESSMENT]
-		self.assessmentHigh = self.corpus[LINES_OF_POS_MID_SCORE_ASSESSMENT : LINES_OF_POS_MID_SCORE_ASSESSMENT + LINES_OF_POS_HIGH_SCORE_ASSESSMENT]
+		self.pos_obs_low = open("./eth_pos_low.txt", "r").readlines()
+		self.pos_obs_mid = open("./eth_pos_mid.txt", "r").readlines()
+		self.pos_obs_high = open("./eth_pos_high.txt", "r").readlines()
+		self.pos_asses_low = open("./eth_pos_asses_low.txt", "r").readlines()
+		self.pos_asses_mid = open("./eth_pos_asses_mid.txt", "r").readlines()
+		self.pos_asses_high = open("./eth_pos_asses_high.txt", "r").readlines()
 
 	def getObservation(self, ethics):
 		# Load corpus if all variants in one dimension are used up
-		if len(self.observationLow) == 0 or len(self.observationMid) == 0 or len(self.observationHigh) == 0:
-			self.loadCorpus()
+		if len(self.pos_obs_low) == 0: 
+			self.pos_obs_low = open("./eth_pos_low.txt", "r").readlines()
+		elif len(self.pos_obs_mid) == 0:
+			self.pos_obs_mid = open("./eth_pos_mid.txt", "r").readlines()
+		elif len(self.pos_obs_high) == 0:
+			self.pos_obs_high = open("./eth_pos_high.txt", "r").readlines()
 
 		# Get Observation
 		if ethics < 0.34:
-			return self.observationLow.pop(randrange(len(self.observationLow))).replace("\n", "")
+			return self.pos_obs_low.pop(randrange(len(self.pos_obs_low))).replace("\n", "")
 		elif ethics >= 0.34 and ethics < 0.67:
-			return self.observationMid.pop(randrange(len(self.observationMid))).replace("\n", "")
+			return self.pos_obs_mid.pop(randrange(len(self.pos_obs_mid))).replace("\n", "")
 		else:
-			return self.observationHigh.pop(randrange(len(self.observationHigh))).replace("\n", "")
+			return self.pos_obs_high.pop(randrange(len(self.pos_obs_high))).replace("\n", "")
 
 	def getAssessment(self, ethics):
 		# Load corpus if all variants in one dimension are used up
-		if len(self.assessmentLow) == 0 or len(self.assessmentMid) == 0 or len(self.assessmentHigh) == 0:
-			self.loadCorpus()
+		if len(self.pos_asses_low) == 0:
+			self.pos_asses_low = open("./eth_pos_asses_low.txt", "r").readlines()
+		elif len(self.pos_asses_mid) == 0:
+			self.pos_asses_mid = open("./eth_pos_asses_mid.txt", "r").readlines()
+		elif len(self.pos_asses_high) == 0:
+			self.pos_asses_high = open("./eth_pos_asses_high.txt", "r").readlines()
 
 		# Get Assessment
 		if ethics < 0.34:
-			return self.assessmentLow.pop(randrange(len(self.assessmentLow))).replace("\n", "")
+			return self.pos_asses_low.pop(randrange(len(self.pos_asses_low))).replace("\n", "")
 		elif ethics >= 0.34 and ethics < 0.67:
-			return self.assessmentMid.pop(randrange(len(self.assessmentMid))).replace("\n", "")
+			return self.pos_asses_mid.pop(randrange(len(self.pos_asses_mid))).replace("\n", "")
 		else:
-			return self.assessmentHigh.pop(randrange(len(self.assessmentHigh))).replace("\n", "")
+			return self.pos_asses_high.pop(randrange(len(self.pos_asses_high))).replace("\n", "")
 
 	"""
 		Reference of replacing escape characters in string:
