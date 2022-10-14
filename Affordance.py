1284e46596f05489a1ce8e7a2d08f59339b90908
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-10-14 18:57:27 +0800

    update

--- Affordance.py
+++ Affordance.py
@@ -8,11 +8,14 @@ sentPos: str = r"/sentences/affordance/Affordance/AffordancePosstream.xlsx"
 
 class FileEngine:
 
-    def returnSent(self) -> tuple:
+    def ExcelreturnSent(self) -> tuple:
         negative: pd.DataFrame = pd.read_excel(sentNeg)
         postive: pd.DataFrame = pd.read_excel(sentPos)
         return (negative, postive)
 
+    def TxtreturnSent(self) -> tuple:
+
+        return
 
 class StayWith(Enum):
     SKILLFUL: tuple = [5, [num / 100 for num in range(83, 100, 1)]]
@@ -35,17 +38,27 @@ class Affordance:
 
     def __init__(self, outerln: float) -> None:
         self.inputIn = outerln
-        self.sentences = FileEngine().returnSent()
+        self.sentences = FileEngine().ExcelreturnSent()
         return
 
     def undefined(self) -> None:
         temp: float = float("{:.2f}".format(self.inputIn))
         for iterator in StayWith:
             if temp in iterator.value[1]:
-                self.appear = iterator.value
+                self.appear = iterator.value()
                 break
 
-    def getAffor(self) -> tuple:
+    """
+    be remind here the getAffor would return a python dataframe to caller
+    """
+
+    def getAffor(self) -> str:
         self.undefined()
-        if self.appear[1] < 3: return self.sentences[0]
-        return tuple([self.appear[1], self.inputIn])
+        print("Hey there! you will receive a pandas dataframe! which type is a pandas.Series."
+              "Here is its column name, you can get wanted sentence buy calling it.\n" +
+              self.sentences[0].columns.tolist() + "\n" +
+              self.sentences[1].columns.tolist())
+        if self.appear[1] < 3:
+            return self.sentences[0].iloc[self.appear[1]]
+        elif self.appear[1] > 3:
+            return self.sentences[1].iloc[self.appear[1] - 3]
