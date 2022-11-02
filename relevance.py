9800cb1bde6e5ccf9b5557842833df0f65c63aa5
BanjiBear <tommychen900217@gmail.com>
2022-11-02 21:07:43 +0800

    Modify Relevance & Valence
    1. Comment out unused functions
    2. Re-design loading corpus function and conditions
    3. Re-design relevance/valence score passing in order to fit looping of Coppélia

--- relevance.py
+++ relevance.py
@@ -11,6 +11,8 @@ class Relevance:
 
     relevance = 0
     input_factor = ""
+
+    p_low, p_mid, p_high, n_low, n_mid, n_high = ([] for i in range(6))
     
     # def __init__(self, relevance, input_factor):
     #     self.relevance = relevance
@@ -28,31 +30,42 @@ class Relevance:
         elif(self.input_factor=="aesthetics"): return "aesthetics/"
         else: return "epistemics/"
 
-    def select_sentence_file(self):
-        if(self.relevance<=0.15): return "n_high"
-        elif(self.relevance<0.35): return "n_mid"
-        elif(self.relevance<0.5): return "n_low"
-        elif(self.relevance<0.65): return "p_low"
-        elif(self.relevance<0.85): return "p_mid"
-        else: return "p_high"
+    # def select_sentence_file(self):
+    #     if(self.relevance<=0.15): return "n_high"
+    #     elif(self.relevance<0.35): return "n_mid"
+    #     elif(self.relevance<0.5): return "n_low"
+    #     elif(self.relevance<0.65): return "p_low"
+    #     elif(self.relevance<0.85): return "p_mid"
+    #     else: return "p_high"
 
-    def loadCorpus(self):
-        corpus_p_low="sentences/"+self.select_factor_dic()+"relevance/p_low.txt"
-        corpus_n_low="sentences/"+self.select_factor_dic()+"relevance/n_low.txt"
-        corpus_p_mid="sentences/"+self.select_factor_dic()+"relevance/p_mid.txt"
-        corpus_n_mid="sentences/"+self.select_factor_dic()+"relevance/n_mid.txt"
-        corpus_p_high="sentences/"+self.select_factor_dic()+"relevance/p_high.txt"
-        corpus_n_high="sentences/"+self.select_factor_dic()+"relevance/n_high.txt"
-        self.p_low = open(corpus_p_low, "r").readlines()
-        self.p_mid = open(corpus_p_mid, "r").readlines()
-        self.p_high = open(corpus_p_high, "r").readlines()
-        self.n_low = open(corpus_n_low, "r").readlines()
-        self.n_mid = open(corpus_n_mid, "r").readlines()
-        self.n_high = open(corpus_n_high, "r").readlines()
+    # def loadCorpus(self):
+    #     corpus_p_low="sentences/"+self.select_factor_dic()+"relevance/p_low.txt"
+    #     corpus_n_low="sentences/"+self.select_factor_dic()+"relevance/n_low.txt"
+    #     corpus_p_mid="sentences/"+self.select_factor_dic()+"relevance/p_mid.txt"
+    #     corpus_n_mid="sentences/"+self.select_factor_dic()+"relevance/n_mid.txt"
+    #     corpus_p_high="sentences/"+self.select_factor_dic()+"relevance/p_high.txt"
+    #     corpus_n_high="sentences/"+self.select_factor_dic()+"relevance/n_high.txt"
+    #     self.p_low = open(corpus_p_low, "r").readlines()
+    #     self.p_mid = open(corpus_p_mid, "r").readlines()
+    #     self.p_high = open(corpus_p_high, "r").readlines()
+    #     self.n_low = open(corpus_n_low, "r").readlines()
+    #     self.n_mid = open(corpus_n_mid, "r").readlines()
+    #     self.n_high = open(corpus_n_high, "r").readlines()
 
     def getrelevance(self):
-        if len(self.p_low) == 0 or len(self.p_mid) == 0 or len(self.p_high) == 0 or len(self.n_low) == 0 or len(self.n_mid) == 0 or len(self.n_high) == 0:
-            self.loadCorpus()
+        if len(self.p_low) == 0: 
+            self.p_low = open("sentences/"+self.select_factor_dic()+"relevance/p_low.txt", "r").readlines()
+        if len(self.p_mid) == 0:
+            self.p_mid = open("sentences/"+self.select_factor_dic()+"relevance/p_mid.txt", "r").readlines()
+        if len(self.p_high) == 0:
+            self.p_high = open("sentences/"+self.select_factor_dic()+"relevance/p_high.txt", "r").readlines()
+        if len(self.n_low) == 0: 
+            self.n_low = open("sentences/"+self.select_factor_dic()+"relevance/n_low.txt", "r").readlines()
+        if len(self.n_mid) == 0:
+            self.n_mid = open("sentences/"+self.select_factor_dic()+"relevance/n_mid.txt", "r").readlines()
+        if len(self.n_high) == 0:
+            self.n_high = open("sentences/"+self.select_factor_dic()+"relevance/n_high.txt", "r").readlines()
+
         if self.relevance < 0.15:
             return self.n_high.pop(randrange(len(self.n_high))).replace("\n", "")
         elif self.relevance < 0.35:
--- valence.py
+++ valence.py
@@ -9,9 +9,10 @@ from random import randrange
 
 class Valence:
 
-
     valence = 0
     input_factor = ""
+
+    p_low, p_mid, p_high, n_low, n_mid, n_high = ([] for i in range(6))
     
     # def __init__(self, valence, input_factor):
     #     self.valence = valence
@@ -29,31 +30,43 @@ class Valence:
         elif(self.input_factor=="aesthetics"): return "aesthetics/"
         else: return "epistemics/"
 
-    def select_sentence_file(self):
-        if(self.valence<=0.15): return "n_high"
-        elif(self.valence<0.35): return "n_mid"
-        elif(self.valence<0.5): return "n_low"
-        elif(self.valence<0.65): return "p_low"
-        elif(self.valence<0.85): return "p_mid"
-        else: return "p_high"
+    # def select_sentence_file(self):
+    #     if(self.valence<=0.15): return "n_high"
+    #     elif(self.valence<0.35): return "n_mid"
+    #     elif(self.valence<0.5): return "n_low"
+    #     elif(self.valence<0.65): return "p_low"
+    #     elif(self.valence<0.85): return "p_mid"
+    #     else: return "p_high"
 
-    def loadCorpus(self):
-        corpus_p_low="sentences/"+self.select_factor_dic()+"valence/p_low.txt"
-        corpus_n_low="sentences/"+self.select_factor_dic()+"valence/n_low.txt"
-        corpus_p_mid="sentences/"+self.select_factor_dic()+"valence/p_mid.txt"
-        corpus_n_mid="sentences/"+self.select_factor_dic()+"valence/n_mid.txt"
-        corpus_p_high="sentences/"+self.select_factor_dic()+"valence/p_high.txt"
-        corpus_n_high="sentences/"+self.select_factor_dic()+"valence/n_high.txt"
-        self.p_low = open(corpus_p_low, "r").readlines()
-        self.p_mid = open(corpus_p_mid, "r").readlines()
-        self.p_high = open(corpus_p_high, "r").readlines()
-        self.n_low = open(corpus_n_low, "r").readlines()
-        self.n_mid = open(corpus_n_mid, "r").readlines()
-        self.n_high = open(corpus_n_high, "r").readlines()
+    # def loadCorpus(self):
+    #     corpus_p_low="sentences/"+self.select_factor_dic()+"valence/p_low.txt"
+    #     corpus_n_low="sentences/"+self.select_factor_dic()+"valence/n_low.txt"
+    #     corpus_p_mid="sentences/"+self.select_factor_dic()+"valence/p_mid.txt"
+    #     corpus_n_mid="sentences/"+self.select_factor_dic()+"valence/n_mid.txt"
+    #     corpus_p_high="sentences/"+self.select_factor_dic()+"valence/p_high.txt"
+    #     corpus_n_high="sentences/"+self.select_factor_dic()+"valence/n_high.txt"
+    #     self.p_low = open(corpus_p_low, "r").readlines()
+    #     self.p_mid = open(corpus_p_mid, "r").readlines()
+    #     self.p_high = open(corpus_p_high, "r").readlines()
+    #     self.n_low = open(corpus_n_low, "r").readlines()
+    #     self.n_mid = open(corpus_n_mid, "r").readlines()
+    #     self.n_high = open(corpus_n_high, "r").readlines()
 
     def getvalence(self):
-        if len(self.p_low) == 0 or len(self.p_mid) == 0 or len(self.p_high) == 0 or len(self.n_low) == 0 or len(self.n_mid) == 0 or len(self.n_high) == 0:
-            self.loadCorpus()
+        if len(self.p_low) == 0: 
+            self.p_low = open("sentences/"+self.select_factor_dic()+"valence/p_low.txt", "r").readlines()
+        if len(self.p_mid) == 0:
+            self.p_mid = open("sentences/"+self.select_factor_dic()+"valence/p_mid.txt", "r").readlines()
+        if len(self.p_high) == 0:
+            self.p_high = open("sentences/"+self.select_factor_dic()+"valence/p_high.txt", "r").readlines()
+        if len(self.n_low) == 0: 
+            self.n_low = open("sentences/"+self.select_factor_dic()+"valence/n_low.txt", "r").readlines()
+        if len(self.n_mid) == 0:
+            self.n_mid = open("sentences/"+self.select_factor_dic()+"valence/n_mid.txt", "r").readlines()
+        if len(self.n_high) == 0:
+            self.n_high = open("sentences/"+self.select_factor_dic()+"valence/n_high.txt", "r").readlines()
+
+
         if self.valence < 0.15:
             return self.n_high.pop(randrange(len(self.n_high))).replace("\n", "")
         elif self.valence < 0.35:
