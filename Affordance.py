5d732dbbdf85854b2a9f05d266205d961e092736
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-11-03 14:45:17 +0800

    sentence Library update

--- Affordance.py
+++ Affordance.py
@@ -2,20 +2,29 @@ import Aesthetics as aest
 from enum import Enum
 import pandas as pd
 
-sentNeg: str = r"/sentences/affordance/Affordance/AffordanceNegstream.xlsx"
-sentPos: str = r"/sentences/affordance/Affordance/AffordancePosstream.xlsx"
+sentNeg: str = r"./sentences/affordance/Affordance/AffordanceNegstream."
+sentPos: str = r"./sentences/affordance/Affordance/AffordancePosstream."
 
 
 class FileEngine:
 
     def ExcelreturnSent(self) -> tuple:
-        negative: pd.DataFrame = pd.read_excel(sentNeg)
-        postive: pd.DataFrame = pd.read_excel(sentPos)
+        negative: pd.DataFrame = pd.read_excel(sentNeg + "xlsx")
+        postive: pd.DataFrame = pd.read_excel(sentPos + "xlsx")
         return (negative, postive)
 
-    def TxtreturnSent(self) -> tuple:
-
-        return
+    def TxtreturnSent(self) -> pd.DataFrame:
+        """pd.read_fwf(sentNeg)"""
+        alltheway: list = []
+        section: list = []
+        with open(sentNeg+"txt", "r") as fp:
+            lines = fp.readline()
+            for line in lines:
+                if line.isspace():
+                    alltheway.append(section)
+                    section.clear()
+                else: section.append(line)
+        return pd.DataFrame(alltheway)
 
 class StayWith(Enum):
     SKILLFUL: tuple = [5, [num / 100 for num in range(83, 100, 1)]]
@@ -38,7 +47,7 @@ class Affordance:
 
     def __init__(self, outerln: float) -> None:
         self.inputIn = outerln
-        self.sentences = FileEngine().ExcelreturnSent()
+        self.sentences = FileEngine().TxtreturnSent()
         return
 
     def undefined(self) -> None:
@@ -56,9 +65,18 @@ class Affordance:
         self.undefined()
         print("Hey there! you will receive a pandas dataframe! which type is a pandas.Series."
               "Here is its column name, you can get wanted sentence buy calling it.\n" +
-              self.sentences[0].columns.tolist() + "\n" +
-              self.sentences[1].columns.tolist())
-        if self.appear[1] < 3:
-            return self.sentences[0].iloc[self.appear[1]]
-        elif self.appear[1] > 3:
-            return self.sentences[1].iloc[self.appear[1] - 3]
+              " ".join(self.sentences[0].columns.to_list()) + "\n" +
+              " ".join(self.sentences[1].columns.tolist())+ "\n\n")
+        if self.appear[0] < 3:
+            return self.sentences[0].iloc[self.appear[0]]
+        elif self.appear[0] > 3:
+            return self.sentences[1].iloc[self.appear[0] - 3]
+
+if __name__ == "__main__":
+    jack = Affordance(0.87)
+    print(jack.getAffor())
+
+def tempSpliter():
+    file: pd.DataFrame = pd.read_excel(r"D:\Code Working Area\Python\SiliconCoppelia\pythonVersion\sentences\affordance\Affordance\AffordanceNegstream.xlsx")
+    for name, val in file.iteritems():
+        pd.to_csv(r"./sentences/affordance/Affordance/AffordanceNeg/" + name + ".txt")
