f89a03a3b7a7efb5182046bad955c5db160a8ef8
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2022-11-03 18:26:46 +0800

    update the ui sentences

--- relevance.py
+++ relevance.py
@@ -30,28 +30,6 @@ class Relevance:
         elif(self.input_factor=="aesthetics"): return "aesthetics/"
         else: return "epistemics/"
 
-    # def select_sentence_file(self):
-    #     if(self.relevance<=0.15): return "n_high"
-    #     elif(self.relevance<0.35): return "n_mid"
-    #     elif(self.relevance<0.5): return "n_low"
-    #     elif(self.relevance<0.65): return "p_low"
-    #     elif(self.relevance<0.85): return "p_mid"
-    #     else: return "p_high"
-
-    # def loadCorpus(self):
-    #     corpus_p_low="sentences/"+self.select_factor_dic()+"relevance/p_low.txt"
-    #     corpus_n_low="sentences/"+self.select_factor_dic()+"relevance/n_low.txt"
-    #     corpus_p_mid="sentences/"+self.select_factor_dic()+"relevance/p_mid.txt"
-    #     corpus_n_mid="sentences/"+self.select_factor_dic()+"relevance/n_mid.txt"
-    #     corpus_p_high="sentences/"+self.select_factor_dic()+"relevance/p_high.txt"
-    #     corpus_n_high="sentences/"+self.select_factor_dic()+"relevance/n_high.txt"
-    #     self.p_low = open(corpus_p_low, "r").readlines()
-    #     self.p_mid = open(corpus_p_mid, "r").readlines()
-    #     self.p_high = open(corpus_p_high, "r").readlines()
-    #     self.n_low = open(corpus_n_low, "r").readlines()
-    #     self.n_mid = open(corpus_n_mid, "r").readlines()
-    #     self.n_high = open(corpus_n_high, "r").readlines()
-
     def getRelevance(self):
         if len(self.p_low) == 0: 
             self.p_low = open("sentences/"+self.select_factor_dic()+"relevance/p_low.txt", "r").readlines()
@@ -78,35 +56,3 @@ class Relevance:
             return self.p_mid.pop(randrange(len(self.p_mid))).replace("\n", "")
         else:
             return self.p_high.pop(randrange(len(self.p_high))).replace("\n", "")
-
-
-# from readFromTxt import get_line_context;
-# from readFromTxt import get_line_num;
-# from readFromTxt import get_random_line;
-
-# class Relevance:
-
-#     def __init__(self, rel, input_factor):
-#         self.rel = rel
-#         self.input_factor=input_factor
-    
-#     def select_factor_dic(self):
-#         if(self.input_factor=="ethics"): return "ethics/"
-#         elif(self.input_factor=="affordance"): return "affordance/"
-#         elif(self.input_factor=="aesthetics"): return "aesthetics/"
-#         else: return "epistemics/"
-    
-#     def select_sentence_file(self):
-#         if(self.rel<=0.15): return "n_high"
-#         elif(self.rel<0.35): return "n_mid"
-#         elif(self.rel<0.5): return "n_low"
-#         elif(self.rel<0.65): return "p_low"
-#         elif(self.rel<0.85): return "p_mid"
-#         else: return "p_high"
-    
-#     def get_sentences_from_file(self):
-#         file_path="sentences/"+self.select_factor_dic()+"relevance/"+self.select_sentence_file()+".txt"
-#         line_num = get_line_num(file_path)
-#         selected_line = get_random_line(line_num)
-#         sentence = get_line_context(file_path, selected_line)
-#         return sentence
--- useIntention.py
+++ useIntention.py
@@ -30,42 +30,34 @@ class UseIntention:
         elif(self.input_factor=="affordance"): return "affordance/"
         elif(self.input_factor=="aesthetics"): return "aesthetics/"
         else: return "epistemics/"
-
-    # def select_sentence_file(self):
-    #     if(self.use_intention<=0.15): return "n_high"
-    #     elif(self.use_intention<0.35): return "n_mid"
-    #     elif(self.use_intention<0.5): return "n_low"
-    #     elif(self.use_intention<0.65): return "p_low"
-    #     elif(self.use_intention<0.85): return "p_mid"
-    #     else: return "p_high"
-
-    # def loadCorpus(self):
-    #     corpus_p_low="sentences/"+self.select_factor_dic()+"use_intention/p_low.txt"
-    #     corpus_n_low="sentences/"+self.select_factor_dic()+"use_intention/n_low.txt"
-    #     corpus_p_mid="sentences/"+self.select_factor_dic()+"use_intention/p_mid.txt"
-    #     corpus_n_mid="sentences/"+self.select_factor_dic()+"use_intention/n_mid.txt"
-    #     corpus_p_high="sentences/"+self.select_factor_dic()+"use_intention/p_high.txt"
-    #     corpus_n_high="sentences/"+self.select_factor_dic()+"use_intention/n_high.txt"
-    #     self.p_low = open(corpus_p_low, "r").readlines()
-    #     self.p_mid = open(corpus_p_mid, "r").readlines()
-    #     self.p_high = open(corpus_p_high, "r").readlines()
-    #     self.n_low = open(corpus_n_low, "r").readlines()
-    #     self.n_mid = open(corpus_n_mid, "r").readlines()
-    #     self.n_high = open(corpus_n_high, "r").readlines()
-
+    
     def getUseIntention(self):
         if len(self.p_low) == 0: 
-            self.p_low = open("sentences/"+self.select_factor_dic()+"use_intention/p_low.txt", "r").readlines()
+            self.p_low = open("sentences/use_intention/p_low.txt", "r").readlines()
         if len(self.p_mid) == 0:
-            self.p_mid = open("sentences/"+self.select_factor_dic()+"use_intention/p_mid.txt", "r").readlines()
+            self.p_mid = open("sentences/use_intention/p_mid.txt", "r").readlines()
         if len(self.p_high) == 0:
-            self.p_high = open("sentences/"+self.select_factor_dic()+"use_intention/p_high.txt", "r").readlines()
+            self.p_high = open("sentences/use_intention/p_high.txt", "r").readlines()
         if len(self.n_low) == 0: 
-            self.n_low = open("sentences/"+self.select_factor_dic()+"use_intention/n_low.txt", "r").readlines()
+            self.n_low = open("sentences/use_intention/n_low.txt", "r").readlines()
         if len(self.n_mid) == 0:
-            self.n_mid = open("sentences/"+self.select_factor_dic()+"use_intention/n_mid.txt", "r").readlines()
+            self.n_mid = open("sentences/use_intention/n_mid.txt", "r").readlines()
         if len(self.n_high) == 0:
-            self.n_high = open("sentences/"+self.select_factor_dic()+"use_intention/n_high.txt", "r").readlines()
+            self.n_high = open("sentences/use_intention/n_high.txt", "r").readlines()
+
+    # def getUseIntention(self):
+    #     if len(self.p_low) == 0: 
+    #         self.p_low = open("sentences/"+self.select_factor_dic()+"use_intention/p_low.txt", "r").readlines()
+    #     if len(self.p_mid) == 0:
+    #         self.p_mid = open("sentences/"+self.select_factor_dic()+"use_intention/p_mid.txt", "r").readlines()
+    #     if len(self.p_high) == 0:
+    #         self.p_high = open("sentences/"+self.select_factor_dic()+"use_intention/p_high.txt", "r").readlines()
+    #     if len(self.n_low) == 0: 
+    #         self.n_low = open("sentences/"+self.select_factor_dic()+"use_intention/n_low.txt", "r").readlines()
+    #     if len(self.n_mid) == 0:
+    #         self.n_mid = open("sentences/"+self.select_factor_dic()+"use_intention/n_mid.txt", "r").readlines()
+    #     if len(self.n_high) == 0:
+    #         self.n_high = open("sentences/"+self.select_factor_dic()+"use_intention/n_high.txt", "r").readlines()
 
         
         if self.use_intention < 0.15:
@@ -81,33 +73,3 @@ class UseIntention:
         else:
             return self.p_high.pop(randrange(len(self.p_high))).replace("\n", "")
 
-# from readFromTxt import get_line_context;
-# from readFromTxt import get_line_num;
-# from readFromTxt import get_random_line;
-
-# class UseIntention:
-
-    # def __init__(self, use_intention, input_factor):
-    #     self.use_intention = use_intention
-    #     self.input_factor=input_factor
-    
-    # def select_factor_dic(self):
-    #     if(self.input_factor=="ethics"): return "ethics/"
-    #     elif(self.input_factor=="affordance"): return "affordance/"
-    #     elif(self.input_factor=="aesthetics"): return "aesthetics/"
-    #     else: return "epistemics/"
-    
-    # def select_sentence_file(self):
-    #     if(self.use_intention<=0.15): return "n_high"
-    #     elif(self.use_intention<0.35): return "n_mid"
-    #     elif(self.use_intention<0.5): return "n_low"
-    #     elif(self.use_intention<0.65): return "p_low"
-    #     elif(self.use_intention<0.85): return "p_mid"
-    #     else: return "p_high"
-    
-    # def get_sentences_from_file(self):
-    #     file_path="sentences/"+self.select_factor_dic()+"use_intention/"+self.select_sentence_file()+".txt"
-    #     line_num = get_line_num(file_path)
-    #     selected_line = get_random_line(line_num)
-    #     sentence = get_line_context(file_path, selected_line)
-    #     return sentence
