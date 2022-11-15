c7dd85c9db1aa3bceeed66feaa8e9bf47ff8c3c6
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-11-16 06:24:31 +0800

    Affordance

--- features/Affordance.py
+++ features/Affordance.py
@@ -1,12 +1,13 @@
-import Aesthetics as aest
 from enum import Enum
 import pandas as pd
 import numpy as np
 import random as rand
+import sys
+sys.path.append("..")
 
-from dimensions import valence as vale
-from dimensions import relevance as reval
-from dimensions import useIntention as ui
+from dimensions.valence import *
+from dimensions.relevance import *
+from dimensions.useIntention import *
 
 initial: str = r"../sentences/affordance/"
 
@@ -20,10 +21,10 @@ __Total_file: int = 11
 
 class FileEngine:
 
-    def ExcelreturnSent(self) -> tuple:
-        negative: pd.DataFrame = pd.read_excel(initial + "Affordance/AffordanceNegstream.xlsx", header=True)
-        positive: pd.DataFrame = pd.read_excel(initial + "Affordance/AffordancePosstream.xlsx", header=True)
-        return (negative, positive)
+    # def ExcelreturnSent(self) -> tuple:
+    #     negative: pd.DataFrame = pd.read_excel(initial + "Affordance/AffordanceNegstream.xlsx", header=True)
+    #     positive: pd.DataFrame = pd.read_excel(initial + "Affordance/AffordancePosstream.xlsx", header=True)
+    #     return negative, positive
 
     def TxtreturnSent(self) -> tuple:
         """pd.read_fwf(sentNeg)"""
@@ -31,11 +32,7 @@ class FileEngine:
                      for rank in suffix[2]]
         neg: list = [pd.read_csv(initial + sentNeg + path + rank + ".txt", header=None, sep='/') for path in suffix[0:2]
                      for rank in suffix[2]]
-        # pos: pd.DataFrame = pd.concat(pos, axis=1)
-        # pos.columns = [*["afforObs" + nuts for nuts in suffix[2]], *["afforAss" + nuts for nuts in suffix[2]]]
-        # neg = pd.concat(neg, axis=1)
-        # neg.columns = [*["afforObs" + nuts for nuts in suffix[2]], *["afforAss" + nuts for nuts in suffix[2]]]
-        return (neg, pos)
+        return neg, pos
 
 
 class StayWith(Enum):
@@ -57,74 +54,82 @@ class Affordance:
     appear: tuple
     sentences: tuple
 
-    def __init__(self, outerln: float) -> None:
+    def __init__(self, outerln: float, attitude: str) -> None:
         self.inputIn = outerln
         self.sentences = FileEngine().TxtreturnSent()
         self.panel: list = [len(self.sentences[0][num]) for num in range(0, len(self.sentences[0]))] + \
                            [len(self.sentences[1][num]) for num in range(0, len(self.sentences[1]))]
-        self.relevance = reval.Relevance()
-        self.valence = vale.Valence()
-        self.useIntention = ui.UseIntention()
+        self.attitude = attitude.lower()
+        self.relevance = Relevance()
+        self.valence = Valence()
+        self.useIntention = UseIntention()
         return
 
     def panelPivot(self, sequence: int, pivot: int) -> int:
         pointer: int = sequence + pivot * 6
-        if not self.panel[pointer]: self.panel[pointer] = [num for num in range(0, len(self.sentences[0]))]
+        if not self.panel[pointer]: self.panel[pointer] = len(self.sentences[pivot][sequence])
         randnum = np.random.randint(self.panel[pointer], size=1)
         self.panel[pointer] -= 1
         return randnum[0]
 
     def undefined(self) -> None:
+        """
+        undefined is used to detect the emotion of Coppelia. to decide further reaction to
+        the target. So this function must be called before modeling
+        """
+        if self.attitude in ["neg", "negative"]: self.inputIn *= -1
         temp: float = float("{:.2f}".format(self.inputIn))
         for iterator in StayWith:
             if temp in iterator.value[1]:
                 self.appear = iterator.value
                 break
 
-    """
-    be remind here the getAffor would return a python dataframe to caller
-    """
-
-    def negSpeaker(self) -> None:
-        print(self.sentences[0][self.appear[0]].iloc[self.panelPivot(self.appear[0], 0)].to_string(header=False, index=False))
-        print(self.sentences[0][self.appear[0] + 3].iloc[self.panelPivot(self.appear[0] + 3, 0)].to_string(header=False, index=False))
+    def negSpeaker(self) -> tuple:
+        """
+        neg_ass, neg_obs are both string sentence
+        """
+        neg_ass = self.sentences[0][self.appear[0]].iloc[self.panelPivot(self.appear[0], 0)].to_string(header=False, index=False)
+        neg_obs = self.sentences[0][self.appear[0] + 3].iloc[self.panelPivot(self.appear[0] + 3, 0)].to_string(header=False, index=False)
+        return neg_ass, neg_obs
         # print(self.relevance.getNegRelevance())
         # print(self.valence.getNegValence())
         # print(self.useIntention.getNegUI())
 
-    def posSpeaker(self) -> None:
-        print(self.sentences[1][self.appear[0] - 3].iloc[self.panelPivot(self.appear[0] - 3, 1)].to_string(header=False, index=False))
-        print(self.sentences[1][self.appear[0]].iloc[self.panelPivot(self.appear[0], 1)].to_string(header=False, index=False))
+    def posSpeaker(self) -> tuple:
+        """
+        pos_ass and pos_obs are both a string sentence, not anything else
+        """
+        pos_ass = self.sentences[1][self.appear[0] - 3].iloc[self.panelPivot(self.appear[0] - 3, 1)].to_string(header=False, index=False)
+        pos_obs = self.sentences[1][self.appear[0]].iloc[self.panelPivot(self.appear[0], 1)].to_string(header=False, index=False)
+        return pos_ass, pos_obs
         # print(self.relevance.getPosRelevance())
         # print(self.valence.getPosValence())
         # print(self.useIntention.getPosUI())
 
-    def affordanceModeling(self) -> None:
-        self.relevance.setInputFactor("affordance")
-        self.valence.setInputFactor("affordance")
-        self.useIntention.setInputFactor("affordance")
-
-        "Oops, due to operation roles are differnet with previous one, the initialization"
-
-        if self.inputIn > 0.66:
-            self.valence.setValence(rand.uniform(0.67, 1))
-            self.useIntention.setUseIntention(rand.uniform(0.67, 1))
-        elif 0.66 >= self.inputIn > 0.33:
-            self.valence.setValence(rand.uniform(0.34, 0.67))
-            self.useIntention.setUseIntention(rand.uniform(0.34, 0.67))
-        else:
-            self.valence.setValence(rand.uniform(0, 0.33))
-            self.useIntention.setUseIntention(rand.uniform(0, 0.33))
-
-    def coppeliaSpeaksEthics(self) -> None:
-        self.undefined()
-        self.affordanceModeling()
-        if self.appear[0] < 3:
-            self.negSpeaker()
-        else:
-            self.posSpeaker()
+    # def affordanceModeling(self) -> None:
+    #     self.relevance.setInputFactor("affordance")
+    #     self.valence.setInputFactor("affordance")
+    #     self.useIntention.setInputFactor("affordance")
+    #
+    #     if self.inputIn > 0.66:
+    #         self.valence.setValence(rand.uniform(0.67, 1))
+    #         self.useIntention.setUseIntention(rand.uniform(0.67, 1))
+    #     elif 0.66 >= self.inputIn > 0.33:
+    #         self.valence.setValence(rand.uniform(0.34, 0.67))
+    #         self.useIntention.setUseIntention(rand.uniform(0.34, 0.67))
+    #     else:
+    #         self.valence.setValence(rand.uniform(0, 0.33))
+    #         self.useIntention.setUseIntention(rand.uniform(0, 0.33))
+    #
+    # def coppeliaSpeaksEthics(self) -> None:
+    #     self.undefined()
+    #     self.affordanceModeling()
+    #     if self.appear[0] < 3:
+    #         self.negSpeaker()
+    #     else:
+    #         self.posSpeaker()
 
 
 if __name__ == "__main__":
-    jack = Affordance(0.9)
-    jack.coppeliaSpeaksEthics()
+    jack = Affordance(0.9, "neg")
+    # jack.coppeliaSpeaksEthics()
