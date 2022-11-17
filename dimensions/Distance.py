309e68f1ca4d4f1e74d082f961dd36a9d37aebf1
BanjiBear <tommychen900217@gmail.com>
2022-11-17 21:44:16 +0800

    Invl, Dist: remove dependency on Ethics and Affordance, simply return a sentence

--- dimensions/Distance.py
+++ dimensions/Distance.py
@@ -1,68 +1,83 @@
 from random import randrange
-import sys
-
-sys.path.append("..")
 
 
 class Distance:
-    distance = 0
-    input_factor = ""
 
-    a_low, a_mid, a_high, e_low, e_mid, e_high = ([] for i in range(6))
+    # a_low, a_mid, a_high, e_low, e_mid, e_high = ([] for i in range(6))
+    dis_high, dis_low, dis_mid = ([] for i in range(3))
 
     def setDistance(self, distance):
         self.distance = distance
 
-    def setInputFactor(self, input_factor):
-        self.input_factor = input_factor
+    def getDistance(self):
+        return self.distance
 
-    def select_factor_dic(self):
-        if self.input_factor == "ethics":
-            return "eth/"
-        elif self.input_factor == "affordance":
-            return "aff/"
-        else:
-            pass
-            # return "aest_pos/"
+    # def setInputFactor(self, input_factor):
+    #     self.input_factor = input_factor
 
-    def getAffordance(self):
+    # def select_factor_dic(self):
+    #     if self.input_factor == "ethics":
+    #         return "eth/"
+    #     elif self.input_factor == "affordance":
+    #         return "aff/"
+    #     else:
+    #         pass
+    #         # return "aest_pos/"
+
+    def getDistance(self):
         # Load corpus if all variants in one dimension are used up
-        if len(self.a_low) == 0:
-            self.a_low = open("../sentences/distance/" + self.select_factor_dic() + "_low.txt", "r").readlines()
-        if len(self.a_mid) == 0:
-            self.a_mid = open("../sentences/distance/" + self.select_factor_dic() + "_mid.txt", "r").readlines()
-        if len(self.a_high) == 0:
-            self.a_high = open("../sentences/distance/" + self.select_factor_dic() + "_high.txt", "r").readlines()
+        if len(self.dis_low) == 0:
+            self.dis_low = open("./sentences/distance/dist_low.txt", "rb").readlines()
+        if len(self.dis_mid) == 0:
+            self.dis_mid = open("./sentences/distance/dist_mid.txt", "rb").readlines()
+        if len(self.dis_high) == 0:
+            self.dis_high = open("./sentences/distance/dist_high.txt", "rb").readlines()
 
         if self.distance < 0.34:
-            return self.a_low.pop(randrange(len(self.a_low))).replace("\n", "")
+            return self.dis_low.pop(randrange(len(self.dis_low))).decode("utf-8")
         elif self.distance < 0.67:
-            return self.a_mid.pop(randrange(len(self.a_mid))).replace("\n", "")
+            return self.dis_mid.pop(randrange(len(self.dis_mid))).decode("utf-8")
         else:
-            return self.a_high.pop(randrange(len(self.a_high))).replace("\n", "")
+            return self.dis_high.pop(randrange(len(self.dis_high))).decode("utf-8")
 
-    def getEthics(self):
-        if len(self.e_low) == 0:
-            self.e_low = open("../sentences/distance/" + self.select_factor_dic() + "_low.txt", "r").readlines()
-        if len(self.e_mid) == 0:
-            self.e_mid = open("../sentences/distance/" + self.select_factor_dic() + "_mid.txt", "r").readlines()
-        if len(self.e_high) == 0:
-            self.e_high = open("../sentences/distance/" + self.select_factor_dic() + "_high.txt", "r").readlines()
+    # def getAffordance(self):
+    #     # Load corpus if all variants in one dimension are used up
+    #     if len(self.a_low) == 0:
+    #         self.a_low = open("../sentences/distance/" + self.select_factor_dic() + "_low.txt", "r").readlines()
+    #     if len(self.a_mid) == 0:
+    #         self.a_mid = open("../sentences/distance/" + self.select_factor_dic() + "_mid.txt", "r").readlines()
+    #     if len(self.a_high) == 0:
+    #         self.a_high = open("../sentences/distance/" + self.select_factor_dic() + "_high.txt", "r").readlines()
 
-        if self.distance < 0.34:
-            return self.e_high.pop(randrange(len(self.e_low))).replace("\n", "")
-        elif self.distance < 0.67:
-            return self.e_mid.pop(randrange(len(self.e_mid))).replace("\n", "")
-        else:
-            return self.e_low.pop(randrange(len(self.e_high))).replace("\n", "")
+    #     if self.distance < 0.34:
+    #         return self.a_low.pop(randrange(len(self.a_low))).replace("\n", "")
+    #     elif self.distance < 0.67:
+    #         return self.a_mid.pop(randrange(len(self.a_mid))).replace("\n", "")
+    #     else:
+    #         return self.a_high.pop(randrange(len(self.a_high))).replace("\n", "")
+
+    # def getEthics(self):
+    #     if len(self.e_low) == 0:
+    #         self.e_low = open("../sentences/distance/" + self.select_factor_dic() + "_low.txt", "r").readlines()
+    #     if len(self.e_mid) == 0:
+    #         self.e_mid = open("../sentences/distance/" + self.select_factor_dic() + "_mid.txt", "r").readlines()
+    #     if len(self.e_high) == 0:
+    #         self.e_high = open("../sentences/distance/" + self.select_factor_dic() + "_high.txt", "r").readlines()
+
+    #     if self.distance < 0.34:
+    #         return self.e_high.pop(randrange(len(self.e_low))).replace("\n", "")
+    #     elif self.distance < 0.67:
+    #         return self.e_mid.pop(randrange(len(self.e_mid))).replace("\n", "")
+    #     else:
+    #         return self.e_low.pop(randrange(len(self.e_high))).replace("\n", "")
 
 
-if __name__ == "__main__":
-    jack = Distance()
-    jack.setInputFactor("affordance")
-    jack.setDistance(0.6)
-    print(jack.getAffordance())
+# if __name__ == "__main__":
+#     jack = Distance()
+#     jack.setInputFactor("affordance")
+#     jack.setDistance(0.6)
+#     print(jack.getAffordance())
 
-    jack.setInputFactor("ethics")
-    jack.setDistance(0.3)
-    print(jack.getAffordance())
+#     jack.setInputFactor("ethics")
+#     jack.setDistance(0.3)
+#     print(jack.getAffordance())
--- dimensions/Involvement.py
+++ dimensions/Involvement.py
@@ -1,45 +1,44 @@
 from random import randrange
-import sys
-sys.path.append("..")
 
 
 class Involvement:
 
-    involvement = 0
-    input_factor = ""
-
-    aff_low, aff_mid, aff_high, eth_low, eth_mid, eth_high = ([] for i in range(6))
+    # aff_low, aff_mid, aff_high, eth_low, eth_mid, eth_high = ([] for i in range(6))
+    invl_high, invl_low, invl_mid = ([] for i in range(3))
 
     def setInvolvement(self, involvement):
         self.involvement = involvement
 
-    def setInputFactor(self, input_factor):
-        self.input_factor = input_factor
+    def getInvolvement(self):
+        return self.involvement
 
-    def select_factor_dic(self):
-        if self.input_factor == "involvement":
-            return "Inv"
-        elif self.input_factor == "ethics":
-            return "eth"
-        else:
-            pass
-            # return "aest_pos/"
+    # def setInputFactor(self, input_factor):
+    #     self.input_factor = input_factor
+
+    # def select_factor_dic(self):
+    #     if self.input_factor == "involvement":
+    #         return "Inv"
+    #     elif self.input_factor == "ethics":
+    #         return "eth"
+    #     else:
+    #         pass
+    #         # return "aest_pos/"
 
     def getInvolvement(self):
         # Load corpus if all variants in one dimension are used up
-        if len(self.aff_low) == 0:
-            self.aff_low = open("./sentences/involvement/" + self.select_factor_dic() + "_low.txt", "rb").readlines()
-        if len(self.aff_mid) == 0:
-            self.aff_mid = open("./sentences/involvement/" + self.select_factor_dic() + "_mid.txt", "rb").readlines()
-        if len(self.aff_high) == 0:
-            self.aff_high = open("./sentences/involvement/" + self.select_factor_dic() + "_high.txt", "rb").readlines()
+        if len(self.invl_low) == 0:
+            self.invl_low = open("./sentences/involvement/invl_low.txt", "rb").readlines()
+        if len(self.invl_mid) == 0:
+            self.invl_mid = open("./sentences/involvement/invl_mid.txt", "rb").readlines()
+        if len(self.invl_high) == 0:
+            self.invl_high = open("./sentences/involvement/invl_high.txt", "rb").readlines()
 
         if self.involvement < 0.34:
-            return self.aff_low.pop(randrange(len(self.aff_low))).decode("utf-8")
+            return self.invl_low.pop(randrange(len(self.invl_low))).decode("utf-8")
         elif self.involvement < 0.67:
-            return self.aff_mid.pop(randrange(len(self.aff_mid))).decode("utf-8")
+            return self.invl_mid.pop(randrange(len(self.invl_mid))).decode("utf-8")
         else:
-            return self.aff_high.pop(randrange(len(self.aff_high))).decode("utf-8")
+            return self.invl_high.pop(randrange(len(self.invl_high))).decode("utf-8")
 
     # def getEthics(self):
     #     # Load corpus if all variants in one dimension are used up
@@ -58,10 +57,10 @@ class Involvement:
     #         return self.eth_high.pop(randrange(len(self.eth_high))).decode("utf-8")
 
 
-if __name__ == "__main__":
-    jack = Involvement()
-    jack.setInputFactor("involvement")
-    jack.setInvolvement(0.6)
-    print(jack.getInvolvement())
+# if __name__ == "__main__":
+#     jack = Involvement()
+#     jack.setInputFactor("involvement")
+#     jack.setInvolvement(0.6)
+#     print(jack.getInvolvement())
 
 
