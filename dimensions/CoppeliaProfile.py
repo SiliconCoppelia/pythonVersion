c45daf470fdd16a01d4d78c16aeb18af35ec4249
BanjiBear <tommychen900217@gmail.com>
2022-12-03 17:08:33 +0800

    deprecate similarity, create Coppelia Profile

---
+++ dimensions/CoppeliaProfile.py
@@ -0,0 +1,51 @@
+from random import random
+from random import randrange
+
+#from abc import ABC, abstractmethod
+
+class CoppeliaProfile:
+
+    COPPELIA_ON_EHTHICS = "Very Kind"
+    COPPELIA_ON_AFFORDANCE = "Smart"
+    PREFERENCE_ON_EthICS = "Friendly"
+    PREFERENCE_ON_AFFORDANCE = "Nutural"
+
+
+    def factorsOnGoals(self, goal):
+        return 
+
+    def similarityOnInvl(self):
+        """
+            calculate the distance between Coppelia and the Agent
+        """
+        return 
+
+    def similarityOnDist(self):
+        """
+            calculate the distance between Coppelia and the Agent
+        """
+        return 
+
+    def similarityOnUI(self):
+        """
+            calculate the distance between Coppelia and the Agent
+        """
+        return 
+
+    def setIvolvement(self, involvement):
+        self.involvement = involvement
+
+    def setDistance(self, distance):
+        self.distance = setDistance
+
+    def setUseIntention(self, useIntention):
+        self.useIntention = setUseIntention
+
+    def getIvolvement(self):
+        return self.involvement
+
+    def getDistance(self):
+        return self.distance
+
+    def getUseIntention(self):
+        return self.useIntention
